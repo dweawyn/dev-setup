7f7ead969e02add5649e7ac92e85a6620abf8563
Jean-Francois Zinque <jzinque@gmail.com>
2017-03-14 10:16:11 +0100

    Fix update.sh for develop branch

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 #Sync if possible
 if [ -d .git ]
 then
-    git pull origin master;
+    git pull origin develop;
 else 
 	echo "dev-setup was not cloned, unable to sync."
 fi
